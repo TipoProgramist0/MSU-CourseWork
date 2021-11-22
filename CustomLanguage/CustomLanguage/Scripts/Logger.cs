8b2a09eaa4063c060b14c3830d0fd981250943e1
TipoProgramist0 <73340494+TipoProgramist0@users.noreply.github.com>
2021-11-22 23:36:54 +0300

    Add files via upload

---
+++ CustomLanguage/CustomLanguage/Scripts/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomLanguage.Scripts
+{
+    public static class Logger
+    {
+        static TextBox consoleTextBox;
+
+        public static void Init(TextBox textBox)
+        {
+            consoleTextBox = textBox;
+        }
+
+        public static void Log(string message)
+        {
+            consoleTextBox.Text += Environment.NewLine + DateTime.Now +">>" + message;
+        }
+    }
+}
